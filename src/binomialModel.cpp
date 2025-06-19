9ec6d604281ab1ab8989875517cb6c05ac64348b
Bhagi-Dhakal <Bhagirdhakal@gmail.com>
2025-06-19 19:09:48 -0400

    Params

--- src/binomialModel.cpp
+++ src/binomialModel.cpp
@@ -4,3 +4,11 @@
     Description: I will implement Binomial Model for option Pricing
 */
 
+
+float S = 41.00; // Stock price in dollars
+float K = 40.00; // Strike Price
+float sigma = 0.30; // Basically historical volitality
+float r = 0.08; // Risk free intrest rate..? 
+float T = 2.00; // time in years 
+float deltaDividends = 0.00;  // amout of dividends stock pays in the contracts life time.. 
+float h = 1.00; // time period 
