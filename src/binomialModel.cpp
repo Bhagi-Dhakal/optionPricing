9d55f7f06b0cf15396bf6e170323dfccb8c1f7e5
Bhagi-Dhakal <Bhagirdhakal@gmail.com>
2025-06-20 23:46:33 -0400

    calculateOptionPrice

--- src/binomialModel.cpp
+++ src/binomialModel.cpp
@@ -5,6 +5,8 @@
 */
 
 
+#include <cmath>
+
 float S = 41.00; // Stock price in dollars
 float K = 40.00; // Strike Price
 float sigma = 0.30; // Basically historical volitality
@@ -12,3 +14,11 @@ float r = 0.08; // Risk free intrest rate..?
 float T = 2.00; // time in years 
 float deltaDividends = 0.00;  // amout of dividends stock pays in the contracts life time.. 
 float h = 1.00; // time period 
+
+
+float calculateOptionPrice() {
+
+
+
+}
+
